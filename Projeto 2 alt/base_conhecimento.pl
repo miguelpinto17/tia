92fd6392d723a583425efe63521f85d84bf85ae7
miguelpinto17 <miguelpaispinto@gmail.com>
2024-05-27 19:24:12 +0100

    parte 2b max

--- Projeto 2 alt/base_conhecimento.pl
+++ Projeto 2 alt/base_conhecimento.pl
@@ -35,6 +35,51 @@ menor_duracao([Tratamento1, Tratamento2 | Restante], TratamentoMaisCurto, MenorD
     ;
         menor_duracao([Tratamento2 | Restante], TratamentoMaisCurto, MenorDuracao)).
 
+% Hill Climbing para encontrar o tratamento mais barato
+hill_climbing(EstadoAtual, MelhorEstado) :-
+    findall(EstadoVizinho, arco(EstadoAtual, EstadoVizinho), Vizinhos),
+    melhor_vizinho(EstadoAtual, Vizinhos, MelhorVizinho),
+    (   MelhorVizinho = EstadoAtual
+    ->  MelhorEstado = EstadoAtual
+    ;   hill_climbing(MelhorVizinho, MelhorEstado)
+    ).
+
+melhor_vizinho(EstadoAtual, [], EstadoAtual).
+melhor_vizinho(EstadoAtual, [Vizinho | Restantes], MelhorVizinho) :-
+    custo(EstadoAtual, CustoAtual),
+    custo(Vizinho, CustoVizinho),
+    melhor_vizinho(EstadoAtual, Restantes, MelhorVizinhoRestantes),
+    custo(MelhorVizinhoRestantes, CustoMelhorRestantes),
+    (   CustoVizinho < CustoAtual,
+        CustoVizinho =< CustoMelhorRestantes
+    ->  MelhorVizinho = Vizinho
+    ;   MelhorVizinho = MelhorVizinhoRestantes).
+
+% Breadth-First Search para encontrar o caminho mais curto em termos de número de tratamentos
+bfs(Inicio, Fim, Caminho) :-
+    bfs([[Inicio]], Fim, Caminho, []).
+
+bfs([[Fim | Resto] | _], Fim, Caminho, _) :-
+    reverse([Fim | Resto], Caminho).
+bfs([[EstadoAtual | Resto] | OutrosCaminhos], Fim, Caminho, Visitados) :-
+    findall([ProximoEstado, EstadoAtual | Resto],
+            (arco(EstadoAtual, ProximoEstado),
+             \+ member(ProximoEstado, [EstadoAtual | Resto]),
+             \+ member(ProximoEstado, Visitados)),
+            NovosCaminhos),
+    append(OutrosCaminhos, NovosCaminhos, CaminhosAtualizados),
+    bfs(CaminhosAtualizados, Fim, Caminho, [EstadoAtual | Visitados]).
+
+% Depth-First Search para encontrar qualquer caminho
+dfs(Inicio, Fim, Caminho) :-
+    dfs(Inicio, Fim, [Inicio], Caminho).
+
+dfs(Fim, Fim, Caminho, Caminho).
+dfs(EstadoAtual, Fim, CaminhoAtual, Caminho) :-
+    arco(EstadoAtual, ProximoEstado),
+    \+ member(ProximoEstado, CaminhoAtual),
+    dfs(ProximoEstado, Fim, [ProximoEstado | CaminhoAtual], Caminho).
+
 % Consultas para encontrar o tratamento mais barato ou mais curto para uma doença
 tratamento_mais_barato_para_doenca(Doenca, Tratamento, Custo) :-
     tratamento_mais_barato(Doenca, Tratamento, Custo).
@@ -42,3 +87,13 @@ tratamento_mais_barato_para_doenca(Doenca, Tratamento, Custo) :-
 tratamento_mais_curto_para_doenca(Doenca, Tratamento, Dias) :-
     tratamento_mais_curto(Doenca, Tratamento, Dias).
 
+tratamento_via_hill_climbing(Doenca, Tratamento, Custo) :-
+    tratamentos_para_doenca(Doenca, [TratamentoInicial | _]),
+    hill_climbing(TratamentoInicial, Tratamento),
+    custo(Tratamento, Custo).
+
+tratamento_via_bfs(Inicio, Fim, Caminho) :-
+    bfs(Inicio, Fim, Caminho).
+
+tratamento_via_dfs(Inicio, Fim, Caminho) :-
+    dfs(Inicio, Fim, Caminho).
--- Projeto 2 alt/interface.pl
+++ Projeto 2 alt/interface.pl
@@ -4,8 +4,11 @@
 % Interface para o utilizador
 interface :-
     writeln('Escolha uma opcao:'),
-    writeln('1. Encontrar tratamento mais barato para uma doenca'),
-    writeln('2. Encontrar tratamento mais rapido para uma doenca'),
+    writeln('1. Encontrar tratamento mais barato para uma doenca (Transicao de Estados)'),
+    writeln('2. Encontrar tratamento mais rapido para uma doenca (Transicao de Estados)'),
+    writeln('3. Encontrar tratamento mais barato para uma doenca (Hill Climbing)'),
+    writeln('4. Encontrar tratamento atraves de BFS (Breadth-First Search)'),
+    writeln('5. Encontrar tratamento atraves de DFS (Depth-First Search)'),
     read(Opcao),
     processar_opcao(Opcao).
 
@@ -14,7 +17,7 @@ processar_opcao(1) :-
     read(Doenca),
     (   tratamento_mais_barato_para_doenca(Doenca, Tratamento, Custo)
     ->  format('O tratamento mais barato para ~w e ~w com um custo de ~w.\n', [Doenca, Tratamento, Custo])
-    ;   format('Desculpe, não há tratamentos disponiveis para ~w.\n', [Doenca])
+    ;   format('Desculpe, nao ha tratamentos disponiveis para ~w.\n', [Doenca])
     ),
     interface.
 processar_opcao(2) :-
@@ -22,11 +25,39 @@ processar_opcao(2) :-
     read(Doenca),
     (   tratamento_mais_curto_para_doenca(Doenca, Tratamento, Dias)
     ->  format('O tratamento mais rapido para ~w e ~w com uma duracao de ~w dias.\n', [Doenca, Tratamento, Dias])
-    ;   format('Desculpe, não ha tratamentos disponiveis para ~w.\n', [Doenca])
+    ;   format('Desculpe, nao ha tratamentos disponiveis para ~w.\n', [Doenca])
+    ),
+    interface.
+processar_opcao(3) :-
+    writeln('Por favor, insira o nome da doenca:'),
+    read(Doenca),
+    (   tratamento_via_hill_climbing(Doenca, Tratamento, Custo)
+    ->  format('O tratamento via Hill Climbing para ~w e ~w com um custo de ~w.\n', [Doenca, Tratamento, Custo])
+    ;   format('Desculpe, nao ha tratamentos disponiveis para ~w.\n', [Doenca])
+    ),
+    interface.
+processar_opcao(4) :-
+    writeln('Por favor, insira o estado clinico inicial:'),
+    read(Inicio),
+    writeln('Por favor, insira o estado clinico final:'),
+    read(Fim),
+    (   tratamento_via_bfs(Inicio, Fim, Caminho)
+    ->  format('O caminho via BFS do estado ~w ao estado ~w e ~w.\n', [Inicio, Fim, Caminho])
+    ;   format('Desculpe, nao ha caminho disponivel do estado ~w ao estado ~w.\n', [Inicio, Fim])
+    ),
+    interface.
+processar_opcao(5) :-
+    writeln('Por favor, insira o estado clinico inicial:'),
+    read(Inicio),
+    writeln('Por favor, insira o estado clinico final:'),
+    read(Fim),
+    (   tratamento_via_dfs(Inicio, Fim, Caminho)
+    ->  format('O caminho via DFS do estado ~w ao estado ~w e ~w.\n', [Inicio, Fim, Caminho])
+    ;   format('Desculpe, nao ha caminho disponivel do estado ~w ao estado ~w.\n', [Inicio, Fim])
     ),
     interface.
 processar_opcao(_) :-
-    writeln('Opção inválida. Por favor, escolha 1, 2 ou 3.'),
+    writeln('Opçao invalida. Por favor, escolha 1, 2, 3, 4 ou 5.'),
     interface.
 
-    :- initialization(interface).
+:- initialization(interface).
